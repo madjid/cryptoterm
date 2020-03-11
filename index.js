2f3dd773bc1242471c85f02fcacda5901a239986
Madjid Mahdjoubi <madjid@orbital-systems.com>
2020-03-11 09:30:54 +0100

    v2 unfinished

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 var request = require('request')
 var Table = require('cli-table')
 const chalk = require('chalk')
+var timestamp = require('unix-timestamp')
 var numeral = require('numeral')
 var args = require('minimist')(process.argv.slice(2))
-var baseurl = 'https://api.coinmarketcap.com/v1/ticker/'
-var globalurl = 'https://api.coinmarketcap.com/v1/global/'
+var baseurl = 'https://api.coinmarketcap.com/v2/ticker/?convert=BTC&limit=3'
+var globalurl = 'https://api.coinmarketcap.com/v2/global/'
 const log = console.log
 var ticker = args.t
 var lim = args.l
@@ -34,20 +35,21 @@ function globalData(callback) {
 
       if (!error && response.statusCode === 200) {
         log(' ')
-        var data = body
-        var totalMarketCap = data.total_market_cap_usd
-        var total24hVolume = data.total_24h_volume_usd
+
+        var data = body.data
+        var totalMarketCap = data.quotes.USD.total_market_cap
+        var total24hVolume = data.quotes.USD.total_volume_24h
         var btcPercentage = data.bitcoin_percentage_of_market_cap
-        var activeCurrencies = data.active_currencies
-        var activeAssets = data.active_assets
+        var activeCurrencies = data.active_cryptocurrencies
         var activeMarkets = data.active_markets
+        var lastUpdated = data.last_updated
 
         var totalMarketCapFormatted = 'Total market cap: ' + chalk.blueBright(formatNumeral(totalMarketCap))
         var total24hVolumeFormatted = 'Total 24h Volume: ' + chalk.blueBright(formatNumeral(total24hVolume))
         var btcPercentageFormatted = 'BTC Percentage: ' + chalk.blueBright(btcPercentage + '%')
         var activeCurrenciesFormatted = 'Active Currencies: ' + chalk.blueBright(activeCurrencies)
-        var activeAssetsFormatteed = 'Active Assets: ' + chalk.blueBright(activeAssets)
         var activeMarketsFormatted = 'Active Markets: ' + chalk.blueBright(activeMarkets)
+        var lastUpdatedFormatteed = 'Last Updated: ' + chalk.blueBright(timestamp.toDate(lastUpdated))
 
         var table = new Table({
           chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
@@ -57,7 +59,7 @@ function globalData(callback) {
         })
         table.push(
           [totalMarketCapFormatted, total24hVolumeFormatted, btcPercentageFormatted],
-          [activeCurrenciesFormatted, activeAssetsFormatteed, activeMarketsFormatted]
+          [activeCurrenciesFormatted, activeMarketsFormatted, lastUpdatedFormatteed]
         )
         log(table.toString())
         callback(response.statusCode, error)
@@ -83,33 +85,57 @@ function tickerData() {
         , colAligns: ['right', 'left', 'right', 'left', 'right', 'right', 'right', 'right', 'right', 'left', 'right', 'right', 'right']
         , style : {compact : true, 'padding-left' : 1, head: ['yellow']}
       })
+          
+    var data = sortObject(body)
+    var currencies = Object.keys(data)
+    
+    currencies.forEach(function(key) {
+      var coin = data[key]
+      var test = coin.id
+      var name = coin.name
+      var rank =  coin.rank
+      var sym = coin.symbol
+      var usd = decimals(coin.quotes.USD.price, 2)
+      
+      var btc = decimals(coin.quotes.BTC.price, 8)
+      
+      /*
+      var btc = coin.quotes.BTC.price
+      if (typeof(btc) == 'undefined'){
+        btc = 'N/A'
+      } else {
 
-  		for (var i = 0; i < limit; i++) {
-        var data = body[i]
-        var name = data.name
-      	var rank =  data.rank
-        var sym = data.symbol
-        var usd = decimals(data.price_usd, 2)
-        var btc = decimals(data.price_btc, 8)
-        var hour = decimals(data.percent_change_1h, 2)
-        var day = decimals(data.percent_change_24h, 2)
-        var week = decimals(data.percent_change_7d, 2)
-        var marketcap = formatNumeral(data.market_cap_usd)
-        var volume = formatNumeral(data['24h_volume_usd'])
-        var availableSupply = formatNumeralUnits(data.available_supply)
-        var totalSupply = formatNumeralUnits(data.total_supply)
-        var maxSupply = formatNumeralUnits(data.max_supply)
-        var tableData = [rank, sym, usd, btc, colorByPercent(hour), colorByPercent(day), colorByPercent(week),
-          colorByNumeral(marketcap), colorByNumeral(volume), name, colorByNumeral(availableSupply), colorByNumeral(totalSupply), colorByNumeral(maxSupply)]
-        var fixedData = replaceNull(tableData)
-        table.push(fixedData)
-  		}
+        btc = decimals(coin.quotes.BTC.price, 8)
+      }  
+*/
+      var hour = decimals(coin.quotes.USD.percent_change_1h, 2)
+      var day = decimals(coin.quotes.USD.percent_change_24h, 2)
+      var week = decimals(coin.quotes.USD.percent_change_7d, 2)
+      var marketcap = formatNumeral(coin.quotes.USD.market_cap)
+      var volume = formatNumeral(coin.quotes.USD.volume_24h)
+      var availableSupply = formatNumeralUnits(coin.circulating_supply)
+      var totalSupply = formatNumeralUnits(coin.total_supply)
+      var maxSupply = formatNumeralUnits(coin.max_supply)
+      var tableData = [rank, sym, usd, btc, colorByPercent(hour), colorByPercent(day), colorByPercent(week),
+        colorByNumeral(marketcap), colorByNumeral(volume), name, colorByNumeral(availableSupply), colorByNumeral(totalSupply), colorByNumeral(maxSupply)]
+      var fixedData = replaceNull(tableData)
+      table.push(fixedData)
+      
+    });
       log(table.toString())
       log(' ')
       } else {
   		log(error)
   	}
   })
+}
+
+function sortObject(o) {
+  return Object.keys(o.data)
+  .sort((a,b) => (o.data[a].rank - o.data[b].rank))
+  .reduce((acc,cur) => {
+    return(acc[o.data[cur].rank] = o.data[cur],acc)
+  },{})
 }
 
 function replaceNull(tableData) {
@@ -151,10 +177,10 @@ function setUrl(ticker, lim) {
     return baseurl + '/' + ticker
   } else if (lim) {
     limit = lim
-    return baseurl + '?limit='+ lim.toString()
+    return baseurl + '&limit='+ lim.toString()
   }
   limit = 10
-  return baseurl + '?limit='+ limit.toString()
+  return baseurl + '&limit='+ limit.toString()
 }
 
 function formatNumeral(number){
